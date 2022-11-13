71c452afde090cee14cc11c2d748e32ef14cb804
Nicolas Wicki <nwicki@ethz.ch>
2022-11-13 23:24:03 +0100

    Final solution

--- task1/train.py
+++ task1/train.py
@@ -1,40 +1,51 @@
+import sklearn.base
 from sklearn.metrics import r2_score
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, SGDRegressor, RANSACRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, SGDRegressor, BayesianRidge
 from sklearn.svm import SVR
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor, AdaBoostRegressor, \
-    BaggingRegressor, HistGradientBoostingRegressor, IsolationForest, StackingRegressor
-from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+    HistGradientBoostingRegressor, IsolationForest, StackingRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV, RandomizedSearchCV
 from sklearn.impute import SimpleImputer, IterativeImputer
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
 from sklearn.feature_selection import SelectPercentile, VarianceThreshold
 import pandas
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
+from catboost import CatBoostRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 import time
 import numpy
+import datetime
 
 TRAINING_DATA_X = ""
 
 
 def StackedModel():
     estimators = [
-        ('kn', KNeighborsRegressor(n_neighbors=12, weights='distance', leaf_size=1, p=1)),
-        ('lgbm', LGBMRegressor(boosting_type='gbdt', num_leaves=32, max_depth=8, n_estimators=2500)),
-        ('gb', GradientBoostingRegressor(n_estimators=500, max_depth=4)),
-        ('svr', SVR(kernel='rbf', C=9, degree=2, tol=1e-2, epsilon=1)),
-        ('rf', RandomForestRegressor(n_estimators=500, max_depth=8, n_jobs=-1)),
-        ('xt', ExtraTreesRegressor(n_estimators=400, max_depth=8, n_jobs=-1)),
-        ('xgb', XGBRegressor(n_estimators=400, max_depth=4, n_jobs=-1)),
-        ('ada', AdaBoostRegressor(n_estimators=2000)),
-        ('mlp', MLPRegressor(alpha=1e-1, early_stopping=True, hidden_layer_sizes=(7, 7, 7, 7, 7, 7, 7, 7),
-                             learning_rate='invscaling', learning_rate_init=0.1, max_iter=int(1e4))),
-        ('hgb', HistGradientBoostingRegressor(max_iter=550, min_samples_leaf=28, max_leaf_nodes=17))
+        # ('kn', KNeighborsRegressor(weights='distance', p=1, n_neighbors=4, n_jobs=-1, leaf_size=1, algorithm='auto')),
+        ('lgbm', LGBMRegressor(
+            subsample_freq=1, subsample_for_bin=400000, subsample=0.5, reg_lambda=0.5, reg_alpha=3, num_leaves=16,
+            n_jobs=-1, n_estimators=3000, min_split_gain=0.025, min_child_weight=2, min_child_samples=8, max_depth=8,
+            learning_rate=0.01, colsample_bytree=0.7, boosting_type='gbdt')),
+        ('gb', GradientBoostingRegressor(
+            subsample=0.9, n_estimators=2250, min_samples_split=0.3, min_samples_leaf=0.05, min_impurity_decrease=0.1,
+            max_depth=9, loss='squared_error', learning_rate=0.01, criterion='friedman_mse')),
+        ('svr', SVR(epsilon=1.5, C=49, cache_size=2048)),
+        # ('rf', RandomForestRegressor(n_estimators=500, max_depth=8, n_jobs=-1)),
+        # ('xt', ExtraTreesRegressor(n_estimators=400, max_depth=8, n_jobs=-1)),
+        ('xgb', XGBRegressor(subsample=0.4, reg_lambda=0.25, reg_alpha=1, n_estimators=3500,
+                             max_depth=7, learning_rate=0.01, colsample_bytree=0.6, n_jobs=-1)),
+        # ('ada', AdaBoostRegressor(n_estimators=2000)),
+        # ('hgb', HistGradientBoostingRegressor(max_iter=550, min_samples_leaf=28, max_leaf_nodes=17)),
+        # ('cat,', CatBoostRegressor(depth=6, learning_rate=0.1, l2_leaf_reg=7, logging_level='Silent')),
+        # ('gp', GaussianProcessRegressor(kernel=DotProduct() + WhiteKernel())),
+        # ('br', BayesianRidge(alpha_1=1e-07, alpha_2=1e-05, lambda_1=1e-05, lambda_2=1e-07, n_iter=300)),
     ]
     return StackingRegressor(estimators=estimators, cv=10, n_jobs=-1)
-
 
 def write_results(model):
     y_pred = model.predict(get_test_data())
@@ -54,7 +65,7 @@ def train(X, y, model):
 
 def get_test_data():
     data = pandas.read_csv("X_test.csv")
-    X = data.loc[:, "x0":].to_numpy()
+    X = data.iloc[:, 1:].to_numpy()
     X = feature_selection_variance(X, reset=False)
     X = impute_missing_values_simple(X, reset=False)
     X = feature_selection_regressor(X, reset=False)
@@ -65,7 +76,7 @@ def get_test_data():
 def read_train_data():
     X_data = pandas.read_csv(TRAINING_DATA_X)
     y_data = pandas.read_csv("y_train.csv")
-    X = X_data.loc[:, "x0":].to_numpy()
+    X = X_data.iloc[:, 1:].to_numpy()
     y = y_data["y"].to_numpy()
     return X, y
 
@@ -82,9 +93,9 @@ def get_preprocessed_data():
 
 def cache_impute_result():
     X_data = pandas.read_csv(TRAINING_DATA_X)
-    X = X_data.loc[:, "x0":].to_numpy()
-    X = impute_missing_values_simple(X)
-    pandas.DataFrame(data=X, columns=X_data.columns[1:]).to_csv("X_train_cached.csv", index=True, index_label='id')
+    X = X_data.iloc[:, 1:].to_numpy()
+    X = impute_missing_values_iterative(X)
+    pandas.DataFrame(data=X, columns=X_data.columns[1:]).to_csv("X_train_cached_estimators_100.csv", index=True, index_label='id')
 
 
 def get_split_data():
@@ -114,23 +125,30 @@ def robust_scale(X, reset=True):
     return ROBUST_SCALER.transform(X)
 
 
-IMPUTER = None
+SIMPLE_IMPUTER = None
 
 
 def impute_missing_values_simple(X, reset=True):
-    global IMPUTER
+    global SIMPLE_IMPUTER
     if reset:
-        IMPUTER = SimpleImputer(strategy="median")
-        IMPUTER.fit(X)
-    return IMPUTER.transform(X)
+        SIMPLE_IMPUTER = SimpleImputer(strategy="median")
+        SIMPLE_IMPUTER.fit(X)
+    return SIMPLE_IMPUTER.transform(X)
 
 
-def impute_missing_values_iterative(X):
-    global IMPUTER
-    if IMPUTER is None:
-        IMPUTER = IterativeImputer(initial_strategy="median")
-        IMPUTER.fit(X)
-    return IMPUTER.transform(X)
+ITERATIVE_IMPUTER = None
+
+
+def impute_missing_values_iterative(X, reset=True):
+    start = time.perf_counter()
+    global ITERATIVE_IMPUTER
+    if reset:
+        ITERATIVE_IMPUTER = IterativeImputer(
+            estimator=ExtraTreesRegressor(n_estimators=100, n_jobs=-1), initial_strategy="median")
+        ITERATIVE_IMPUTER.fit(X)
+    end = time.perf_counter()
+    print(f'Iterative imputation - runtime: {end-start} s')
+    return ITERATIVE_IMPUTER.transform(X)
 
 
 def remove_outliers(X, y, outliers=0.0625):
@@ -202,34 +220,45 @@ def feature_selection_regressor(X, y=None, threshold=0.1, reset=True):
 # ('AdaBoostRegressor', -1, 0.4876988170828006)
 # ('MLPRegressor', -1, 0.4233371020725281)
 
-# StackingRegressor SimpleImputer
-# 727.248965251958
-# 0.5576333296254736
-
-# StackingRegressor IterativeImputer
-# 778.5039008309832
-# 0.48661298297559813
-
-
 def main():
     global TRAINING_DATA_X
-    TRAINING_DATA_X = "X_train.csv"
-    # start = time.perf_counter()
-    # X, y = get_preprocessed_data()
-    # model = StackedModel()
-    # scores = cross_validate(model, X, y, cv=10, scoring='r2', n_jobs=-1)["test_score"]
-    # end = time.perf_counter()
-    # print(end - start)
-    # print(sum(scores) / len(scores))
+    TRAINING_DATA_X = "X_train_cached_estimators_10.csv"
+    print(f'Start time: {datetime.datetime.now().time()}')
+    start = time.perf_counter()
     model = StackedModel()
+    name = type(model).__name__
+    # X, y = get_preprocessed_data()
+    # scores = cross_validate(model, X, y, cv=10, scoring='r2', n_jobs=-1)["test_score"]
+    # print(f'Validation Score: {sum(scores) / len(scores)}')
     model.fit(*get_preprocessed_data())
     write_results(model)
-    # model = XGBRegressor(n_jobs=-1)
-    # 'n_estimators': [150, 200, 250, 300], 'max_depth': [2, 4, 8, 16, 32, 64]
-    # clf = GridSearchCV(model, { 'n_estimators': [150], 'max_depth': [2], }, verbose=10, n_jobs=-1, scoring='r2')
+    end = time.perf_counter()
+    print(f'{name} Runtime: {end - start} s')
+    # model_start = time.perf_counter()
+    # model = CatBoostRegressor(logging_level='Silent')
+    # model.fit(X, y)
+    # model_end = time.perf_counter()
+    # run = model_end - model_start
+    # print(f'{name} - Sample Execution Runtime: {run} s')
+    # model = sklearn.base.clone(model)
+    # param_dict = {
+    #     'loss_function': ['RMSE'],
+    #     'iterations': [10],
+    #     'learning_rate': [1e-2],
+    #     'l2_leaf_reg': [3],
+    #     'bootstrap_type': ['Bayesian'],
+    #     'max_depth': [6],
+    #     'min_data_in_leaf': [1],
+    #     'max_leaves': [31],
+    #     'leaf_estimation_method': ['Newton'],
+    # }
+    # n_iter = 5
+    # clf = RandomizedSearchCV(model, param_dict, n_iter=n_iter, verbose=10, n_jobs=-1, scoring='r2')
     # clf.fit(X, y)
-    # print(type(model).__name__, clf.best_score_)
-    # print(type(model).__name__, clf.best_params_)
+    # end = time.perf_counter()
+    # print(f'{name} - RandomizedSearch Runtime: {end - start} s')
+    # print(f'{name} Best Score: {clf.best_score_}')
+    # print(f'{name} Best Parameters: {clf.best_params_}')
     # stats = []
     # param_space = [0.05, 0.0625, 0.075, 0.0875, 0.1]
     # for param in param_space:
